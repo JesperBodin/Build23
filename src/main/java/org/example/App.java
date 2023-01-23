083865ea92fd32ebe675eda18cf8428f1d37350b
Jesper Bodin <jesperbcn@gmail.com>
2023-01-23 15:34:22 +0100

    Fetch Triangle and Point

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.util.Objects;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello There!");
     }
+
+	private int k = 0;
+
+	public int getK(){
+		return k;
+	}
+
+	public void setK(int k){
+		if(k < 0)
+			throw new IllegalArgumentException();
+		this.k =k;
+	}
+
+	public static int add (int a, int b){
+		return a + b;
+	}
+
+	public static boolean odd(int a){
+		if (a % 2 ==1){
+			return true;
+		}
+		else
+			return false;
+	}
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,12 +1,58 @@
 package org.example;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AppTest {
-    @Test
-    void test() {
-        assertThat(true).isTrue();
-    }
+class AppTest
+{
+	@Test
+	void test()
+	{
+		assertTrue(true);
+	}
+
+	@Test
+	void addTwoNumbersReturnSum()
+	{
+		System.out.println(App.add(0, 0));
+		System.out.println(App.add(1, 0));
+		System.out.println(App.add(1, 1));
+		System.out.println(App.add(2, 2));
+
+		var result = App.add(1, 1);
+
+		assertEquals(2, result);
+	}
+
+	@Test
+	void givenOddNumberOddShouldReturnTrue()
+	{
+		assertTrue(App.odd(3));
+	}
+
+
+	@Test
+	void givenEvenNumberOddShouldReturnFalse()
+	{
+		assertTrue(App.odd(2));
+	}
+
+	@Test
+	void setNegativeNumberShouldThrowIllegalArgumentException()
+	{
+		App app = new App();
+
+		assertThrows(IllegalArgumentException.class, () -> app.setK(-1));
+
+
+	}
+
+	@Test
+	void equalsContractForApp(){
+		EqualsVerifier.simple().forClass(App.class).verify();
+	}
 }
